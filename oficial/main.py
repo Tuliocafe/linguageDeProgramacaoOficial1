f5d68e0a4f0c4d4da629e5f99f76fb937136aef7
Tuliocafe <74936595+Tuliocafe@users.noreply.github.com>
2021-04-09 11:22:47 -0300

    Update main.py
    Fabricio a alteração proposta está faz parte da Oficial 1, estou colocando ela no meu repositório também.
    Marco Tulio Paiva Cafe ADS Noite Pitagoras

--- oficial/main.py
+++ oficial/main.py
@@ -23,4 +23,20 @@ if __name__ == '__main__':
     print(courses_name)
     print(type(courses_name))
 
+    # Coloca a lista em ordem alfabetica e remove as duplicatas
+    cursos = sorted(set(courses_name))
+    print(cursos)
+    print(f'Foram cadastrados {len(cursos)} cursos')
+
+    # Tambem fiz o codigo manual caso prefira ver como funciona em um for
+    # cursos = []
+    # courses_name.sort()
+
+    # for c in range(1, len(courses_name)):
+    #     if c == 1:
+    #         cursos.append(courses_name[c])
+    #     elif courses_name[c] != courses_name[c - 1]:
+    #         cursos.append(courses_name[c])
+
+    # print(cursos)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
